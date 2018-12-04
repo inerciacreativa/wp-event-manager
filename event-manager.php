580d40c238968406266b3fd6f98db271ce626aaf
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2018-12-04 22:47:49 +0100

    Updated version

--- event-manager.php
+++ event-manager.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ic Event Manager
  * Plugin URI:  https://github.com/inerciacreativa/wp-event-manager
- * Version:     2.0.9
+ * Version:     2.0.10
  * Text Domain: ic-event-manager
  * Domain Path: /languages
  * Description: Sencillo gestor de eventos.
