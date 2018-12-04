38ec67f8b68e92cffdf095eb5735a5dbf9da444c
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2018-12-04 22:47:15 +0100

    Updated JS and CSS to work with 5.X release of ACF

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,17 +1,28 @@
 ;
 (function ($) {
+  var
+    date_end_show = $('.acf-field[data-name="date_end_show"]'),
+    date_end_show_label = date_end_show.find('label'),
+    date_end_show_input = date_end_show.find('input[type="checkbox"]'),
+    date_end = $('.acf-field[data-name="date_end"], .acf-field[data-name="time_end_hour"], .acf-field[data-name="time_end_minutes"]');
 
-    $('#acf-date_allday .label label').on('click', function () {
-        var checkbox = $(this).parent().next().find('input[type="checkbox"]');
+  date_end_show_label.on('click', function () {
+    if (date_end_show_input.is(':checked')) {
+      date_end.each(function () {
+        $(this).show();
+      });
+    } else {
+      date_end.each(function () {
+        $(this).hide();
+      });
+    }
+  });
 
-        checkbox.click();
+  $('.acf-postbox select').each(function () {
+    $(this).find('option').each(function () {
+      var option = $(this);
+      option.text(option.val());
     });
-
-    $('.acf_postbox select').each(function () {
-        $(this).find('option').each(function () {
-            var option = $(this);
-            option.text(option.val());
-        });
-    });
+  });
 
 }(jQuery));
