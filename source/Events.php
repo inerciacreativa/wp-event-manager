4a2663fdac6744dfe1544df4de31fd30e80b8ea3
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2018-12-06 06:09:54 +0100

    Removed the code for the calendar generation

--- source/Events.php
+++ source/Events.php
@@ -2,10 +2,9 @@
 
 namespace ic\Plugin\EventManager;
 
-use ic\Framework\Plugin\PluginClassDecorator;
-use ic\Framework\Support\Arr;
+use ic\Framework\Hook\HookDecorator;
 use ic\Framework\Support\Date;
-use ic\Framework\Support\Str;
+use ic\Framework\Support\Options;
 
 /**
  * Class Events
@@ -15,7 +14,12 @@ use ic\Framework\Support\Str;
 class Events
 {
 
-	use PluginClassDecorator;
+	use HookDecorator;
+
+	/**
+	 * @var Options
+	 */
+	private $options;
 
 	/**
 	 * @param EventManager $plugin
@@ -40,30 +44,7 @@ class Events
 	 */
 	public function __construct(EventManager $plugin)
 	{
-		$this->setPlugin($plugin);
-	}
-
-	/**
-	 * @return array
-	 */
-	public function calendar(): array
-	{
-		$name     = get_bloginfo('name');
-		$language = explode('-', get_bloginfo('language'));
-		$filename = urlencode(strtolower($name . '-' . reset($language)) . '.ics');
-		$events   = $this->events($this->getOption('calendar.image'), $this->getOption('calendar.limit'));
-		$contents = $this->format($this->serialize([
-			'BEGIN'        => 'VCALENDAR',
-			'PRODID'       => sprintf('-//%s/Calendar %s//EN', $this->name(), $this->version()),
-			'VERSION'      => '2.0',
-			'CALSCALE'     => 'GREGORIAN',
-			'METHOD'       => 'PUBLISH',
-			'X-WR-CALNAME' => $this->getOption('calendar.name') ? $name : '',
-			0              => implode('', $events),
-			'END'          => 'VCALENDAR',
-		]));
-
-		return compact('filename', 'contents');
+		$this->options = $plugin->getOptions();
 	}
 
 	/**
@@ -86,7 +67,8 @@ class Events
 			}
 		}
 
-		$events = $this->hook()->apply('ic_event_manager_events', $events);
+		$events = $this->hook()
+		               ->apply('ic_event_manager_events', $events);
 
 		return $events;
 	}
@@ -109,7 +91,8 @@ class Events
 			$arguments = array_merge($arguments, [
 				'meta_key'     => 'date_end',
 				'meta_compare' => '>=',
-				'meta_value'   => Date::now()->format('Ymd'),
+				'meta_value'   => Date::now()
+				                      ->format('Ymd'),
 			]);
 		} else {
 			$arguments[EventManager::QUERY_ALL] = true;
@@ -126,153 +109,7 @@ class Events
 	 */
 	public function event(\WP_Post $post, string $image): ?Event
 	{
-		$fields = get_fields($post->ID);
-
-		if (empty($fields)) {
-			return null;
-		}
-
-		$fields['web'] = $fields['url'];
-		unset($fields['url']);
-
-		$event = array_merge([
-			'uid'         => $post->ID,
-			'uri'         => home_url('?p=' . $post->ID),
-			'link'        => get_permalink($post),
-			'description' => html_entity_decode(get_the_title($post), ENT_HTML5 | ENT_QUOTES),
-			'summary'     => $this->getSummary($post),
-			'organizer'   => $this->getOrganizer($post),
-			'image'       => $this->getImage($post, $image),
-		], $fields);
-
-		return new Event($this, $event);
-
-	}
-
-	/**
-	 * @param \WP_Post $post
-	 *
-	 * @return string
-	 */
-	protected function getSummary(\WP_Post $post): string
-	{
-		$summary = strip_shortcodes($post->post_content);
-		$summary = apply_filters('the_content', $summary);
-		$summary = strip_tags($summary);
-
-		return $summary;
-	}
-
-	/**
-	 * @param \WP_Post $post
-	 *
-	 * @return string
-	 */
-	protected function getOrganizer(\WP_Post $post): string
-	{
-		if ($this->getOption('organizer.enable')) {
-			$terms = get_the_terms($post, EventManager::TAX_TYPE);
-
-			if (\is_array($terms)) {
-				return $terms[0]->name;
-			}
-
-			return $this->getOption('organizer.default');
-		}
-
-		return '';
-	}
-
-	/**
-	 * @param \WP_Post $post
-	 * @param string   $size
-	 *
-	 * @return \stdClass
-	 */
-	protected function getImage(\WP_Post $post, string $size): ?\stdClass
-	{
-		$id = get_post_meta($post->ID, '_thumbnail_id', true);
-
-		if (!$id) {
-			return null;
-		}
-
-		$image = wp_get_attachment_image_src($id, $size);
-
-		if (!$image) {
-			return null;
-		}
-
-		return (object) [
-			'src'    => $image[0],
-			'width'  => $image[1],
-			'height' => $image[2],
-		];
-	}
-
-	/**
-	 * @param array $input
-	 *
-	 * @return string
-	 */
-	public function serialize(array $input): string
-	{
-		$serialized = Arr::map($input, function ($key, $value) {
-			if (empty($value)) {
-				return '';
-			}
-
-			if (is_numeric($key)) {
-				return $value;
-			}
-
-			return "$key:$value\r\n";
-		});
-
-		return implode('', $serialized);
-	}
-
-	/**
-	 * @param string $input
-	 * @param int    $length
-	 *
-	 * @return string
-	 */
-	protected function format(string $input, int $length = 74): string
-	{
-		$output   = '';
-		$position = 0;
-
-		while ($position < Str::length($input)) {
-			// find the newline
-			$newline = Str::search($input, "\n", $position + 1);
-
-			if (!$newline) {
-				$newline = Str::length($input);
-			}
-
-			$line = Str::substring($input, $position, $newline - $position);
-
-			if (Str::length($line) <= $length) {
-				$output .= $line;
-			} else {
-				// First line cut-off limit is $lineLimit
-				$output .= Str::substring($line, 0, $length);
-				$line   = Str::substring($line, $length);
-
-				// Subsequent line cut-off limit is $lineLimit - 1 due to the leading white space
-				$output .= "\n " . Str::substring($line, 0, $length - 1);
-
-				while (Str::length($line) > $length - 1) {
-					$line   = Str::substring($line, $length - 1);
-					$output .= "\n " . Str::substring($line, 0, $length - 1);
-				}
-			}
-
-			$position = $newline;
-		}
-
-		return $output;
+		return new Event($post, $image, $this->options);
 	}
 
 }
