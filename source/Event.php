d5548af2f5c195a589f7ddf23da4317252ea1aee
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2018-12-06 06:08:43 +0100

    Better separation of concerns

--- source/Event.php
+++ source/Event.php
@@ -3,6 +3,7 @@
 namespace ic\Plugin\EventManager;
 
 use ic\Framework\Support\Date;
+use ic\Framework\Support\Options;
 use ic\Framework\Support\Str;
 use ic\Framework\Support\TextLimiter;
 
@@ -10,19 +11,40 @@ use ic\Framework\Support\TextLimiter;
  * Class Event
  *
  * @package ic\Plugin\EventManager
+ *
+ * @property-read \WP_Post    $post
+ * @property-read int         $uid
+ * @property-read string      $name
+ * @property-read string      $uri
+ * @property-read string      $link
+ * @property-read string      $description
+ * @property-read string      $summary
+ * @property-read object|null $image
+ * @property-read Date        $dateStamp
+ * @property-read Date        $startDate
+ * @property-read Date        $endDate
+ * @property-read array       $location
+ * @property-read string      $organizer
+ * @property-read string      $web
+ *
  */
 class Event
 {
 
 	/**
-	 * @var Events
+	 * @var \WP_Post
 	 */
-	private $calendar;
+	private $post;
 
 	/**
 	 * @var int
 	 */
 	private $uid;
+
+	/**
+	 * @var string
+	 */
+	private $name;
 
 	/**
 	 * @var string
@@ -82,39 +104,36 @@ class Event
 	/**
 	 * CalendarEvent constructor.
 	 *
-	 * @param Events $calendar
-	 * @param array  $event  {
-	 *
-	 * @type int     $uid
-	 * @type string  $uri
-	 * @type string  $description
-	 * @type string  $summary
-	 * @type string  $organizer
-	 * @type string  $image
-	 * @type string  $date_start
-	 * @type string  $date_end
-	 * @type string  $date_allday
-	 * @type string  $venue
-	 * @type string  $address
-	 * @type string  $city
-	 * @type string  $web
-	 *                       }
+	 * @param \WP_Post $post
+	 * @param string   $image
+	 * @param Options  $options
 	 */
-	public function __construct(Events $calendar, array $event)
+	public function __construct(\WP_Post $post, string $image, Options $options)
 	{
-		$this->calendar    = $calendar;
-		$this->uid         = $event['uid'];
+		if ($post->post_type !== EventManager::POST_TYPE) {
+			throw new \InvalidArgumentException(sprintf('The post has an incorrect post type "%s" (ID %d).', $post->post_type, $post->ID));
+		}
+
+		$event = get_fields($post->ID);
+
+		if (empty($event)) {
+			throw new \RuntimeException(sprintf('There are no event information for the post (ID %d).', $post->ID));
+		}
+
+		$this->post        = $post;
+		$this->uid         = $post->ID;
+		$this->name        = $post->post_name;
+		$this->uri         = home_url('?p=' . $post->ID);
+		$this->link        = get_permalink($post);
+		$this->description = html_entity_decode(get_the_title($post), ENT_HTML5 | ENT_QUOTES);
 		$this->dateStamp   = Date::now();
-		$this->startDate   = $this->startDate($event);
-		$this->endDate     = $this->endDate($event);
-		$this->uri         = $event['uri'];
-		$this->link        = $event['link'];
-		$this->description = Str::whitespace($event['description']);
-		$this->summary     = $this->summary($event['summary']);
-		$this->location    = $this->location($event);
-		$this->organizer   = $event['organizer'];
-		$this->image       = $event['image'];
-		$this->web         = $event['web'];
+		$this->startDate   = self::getStartDate($event);
+		$this->endDate     = self::getEndDate($event);
+		$this->location    = self::getLocation($event);
+		$this->summary     = self::getSummary($post);
+		$this->organizer   = self::getOrganizer($post, $options);
+		$this->image       = self::getImage($post, $image);
+		$this->web         = $event['url'];
 	}
 
 	/**
@@ -152,44 +171,11 @@ class Event
 	}
 
 	/**
-	 * @return string
-	 */
-	public function __toString()
-	{
-		$event = $this->event();
-
-		return $this->calendar->serialize($event);
-	}
-
-	/**
-	 * @return array
-	 */
-	public function event(): array
-	{
-		return array_filter([
-			'BEGIN'         => 'VEVENT',
-			'UID'           => $this->uid,
-			'DTSTART'       => $this->startDate->format('Ymd\THis\Z'),
-			'DTEND'         => $this->endDate->format('Ymd\THis\Z'),
-			'DTSTAMP'       => $this->dateStamp->format('Ymd\THis\Z'),
-			'URL;VALUE=URI' => $this->uri,
-			'URL'           => $this->web,
-			'DESCRIPTION'   => $this->encode($this->description),
-			'SUMMARY'       => $this->encode($this->summary),
-			'LOCATION'      => empty($this->location) ? null : $this->encode(implode(' — ', $this->location)),
-			'ORGANIZER'     => $this->encode($this->organizer),
-			'ATTACH'        => $this->image ? $this->image->src : null,
-			'TRANSP'        => 'OPAQUE',
-			'END'           => 'VEVENT',
-		]);
-	}
-
-	/**
 	 * @param array $event
 	 *
 	 * @return Date
 	 */
-	private function startDate(array $event): Date
+	protected static function getStartDate(array $event): Date
 	{
 		$date = mysql2date('Y-m-d', $event['date_start']);
 		$time = $event['date_allday'] ? '00:00:00' : sprintf('%d:%d:00', $event['time_start_hour'], $event['time_start_minutes']);
@@ -202,7 +188,7 @@ class Event
 	 *
 	 * @return Date
 	 */
-	private function endDate(array $event): Date
+	protected static function getEndDate(array $event): Date
 	{
 		if (empty($event['date_end']) || ($event['date_end'] === $event['date_start'])) {
 			$date = mysql2date('Y-m-d', $event['date_start']);
@@ -216,14 +202,30 @@ class Event
 	}
 
 	/**
-	 * @param string $summary
+	 * @param array $event
+	 *
+	 * @return array
+	 */
+	protected static function getLocation(array $event): array
+	{
+		$keys     = array_flip(['venue', 'address', 'city']);
+		$location = array_intersect_key(array_replace($keys, $event), $keys);
+		$location = array_filter($location);
+
+		return $location;
+	}
+
+	/**
+	 * @param \WP_Post $post
 	 *
 	 * @return string
 	 */
-	private function summary(string $summary): string
+	protected static function getSummary(\WP_Post $post): string
 	{
-		$summary = Str::whitespace($summary);
-		$summary = trim($summary);
+		$summary = strip_shortcodes($post->post_content);
+		$summary = apply_filters('the_content', $summary);
+		$summary = strip_tags($summary);
+		$summary = trim(Str::whitespace($summary));
 
 		if (empty($summary)) {
 			return '';
@@ -240,30 +242,51 @@ class Event
 	}
 
 	/**
-	 * @param array $event
+	 * @param \WP_Post $post
+	 * @param string   $size
 	 *
-	 * @return array
+	 * @return \stdClass
 	 */
-	private function location(array $event): array
+	protected static function getImage(\WP_Post $post, string $size): ?\stdClass
 	{
-		$keys     = array_flip(['venue', 'address', 'city']);
-		$location = array_intersect_key(array_replace($keys, $event), $keys);
-		$location = array_filter($location);
+		$id = get_post_meta($post->ID, '_thumbnail_id', true);
 
-		return $location;
+		if (!$id) {
+			return null;
+		}
+
+		$image = wp_get_attachment_image_src($id, $size);
+
+		if (!$image) {
+			return null;
+		}
+
+		return (object) [
+			'src'    => $image[0],
+			'width'  => $image[1],
+			'height' => $image[2],
+		];
 	}
 
 	/**
-	 * @param string $input
+	 * @param \WP_Post $post
+	 * @param Options  $options
 	 *
 	 * @return string
 	 */
-	private function encode(string $input): string
+	protected static function getOrganizer(\WP_Post $post, Options $options): string
 	{
-		$input = preg_replace('/([\,;])/', '\\\$1', $input);
-		$input = str_replace(["\n", "\r"], ["\\n", "\\r"], $input);
+		if ($options->get('organizer.enable')) {
+			$terms = get_the_terms($post, EventManager::TAX_TYPE);
 
-		return $input;
+			if (\is_array($terms)) {
+				return $terms[0]->name;
+			}
+
+			return $options->get('organizer.default');
+		}
+
+		return '';
 	}
 
 }
